bde2fa20ed62b4400f2c571a8b33e388d598a505
Karen Dantas <karennobre74@gmail.com>
2024-04-02 12:55:42 -0300

    add nativewindcss and _layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,11 @@
-import {View} from "react-native" 
+import {View, Text, TextInput} from "react-native" 
 
 export default function Home (){
     return (
-        <View> Olá, mundo! </View>
+        <View className = "bg-green-500 flex-1 justify-center items-center"> 
+            <Text className="text-slate-50 font-bold"> Teste </Text>
+            <TextInput/>
+
+        </View>
     )
 }
